593fdc7c1ad0113780cd45cfd9aa64f0d903d472
Adrien Poupard <adrien@smartb.city>
2022-02-28 22:47:12 +0100

    Allow options http request on f2 http

--- f2-client/f2-client-ktor/f2-client-ktor-http/src/jvmMain/kotlin/f2/client/ktor/http/HttpF2Client.kt
+++ f2-client/f2-client-ktor/f2-client-ktor-http/src/jvmMain/kotlin/f2/client/ktor/http/HttpF2Client.kt
@@ -11,15 +11,33 @@ import io.ktor.http.contentType
 import io.ktor.http.ContentType
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
 
 actual open class HttpF2Client(
 	private val httpClient: HttpClient,
 	private val urlBase: String,
 ) : F2Client {
 
+	private val json: Json = Json {
+		ignoreUnknownKeys = true
+	}
+
+	private fun handlePayloadResponse(json: JsonElement): List<String> {
+		return when (json) {
+			is JsonPrimitive -> listOf(json.toString())
+			is JsonArray -> json.map { it.toString() }
+			else -> listOf(json.toString())
+		}
+	}
+
 	actual override fun supplier(route: String) = F2Supplier<String> {
 		flow {
-			httpClient.get<List<String>>("$urlBase/${route}").forEach {
+			httpClient.get<JsonElement>("$urlBase/${route}").let { element ->
+				handlePayloadResponse(element)
+			}.forEach {
 				emit(it)
 			}
 		}
@@ -27,9 +45,11 @@ actual open class HttpF2Client(
 
 	actual override fun function(route: String) = F2Function<String, String> { msg ->
 		flow {
-			httpClient.post<List<String>>("$urlBase/${route}") {
+			httpClient.post<JsonElement>("$urlBase/${route}") {
 				contentType(ContentType.Application.Json)
 				body = msg.toList()
+			}.let { element ->
+				handlePayloadResponse(element)
 			}.forEach {
 				emit(it)
 			}
