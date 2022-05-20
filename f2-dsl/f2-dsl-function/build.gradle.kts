6e5747aae2f9d7303ff95fbd151c6ca1e6aab684
Adrien Poupard <adrien@smartb.city>
2022-05-20 21:09:36 +0200

    Use last npm publish version

--- f2-dsl/f2-dsl-function/build.gradle.kts
+++ f2-dsl/f2-dsl-function/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("city.smartb.fixers.gradle.kotlin.mpp")
     id("city.smartb.fixers.gradle.publish")
-    id("dev.petuska.npm.publish")
+//    id("dev.petuska.npm.publish")
 }
 
 dependencies {
-//    jsApi("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${city.smartb.gradle.dependencies.FixersVersions.Kotlin.coroutines}")
     commonMainApi("org.jetbrains.kotlinx:kotlinx-coroutines-core:${city.smartb.gradle.dependencies.FixersVersions.Kotlin.coroutines}")
 }
