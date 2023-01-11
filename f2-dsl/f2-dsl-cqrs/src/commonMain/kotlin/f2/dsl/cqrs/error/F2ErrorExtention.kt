116fccfb2cbff8188fa9b0eb10441714d8fdfcd7
Adrien Poupard <adrien@smartb.city>
2023-01-11 22:51:00 +0100

    Clean F2Error extention

--- f2-dsl/f2-dsl-cqrs/src/commonMain/kotlin/f2/dsl/cqrs/error/F2ErrorExtention.kt
+++ f2-dsl/f2-dsl-cqrs/src/commonMain/kotlin/f2/dsl/cqrs/error/F2ErrorExtention.kt
@@ -2,10 +2,14 @@ package f2.dsl.cqrs.error
 
 import f2.dsl.cqrs.exception.F2Exception
 
+inline fun F2ErrorDTO.asException() = asException(null)
+
 inline fun F2ErrorDTO.asException(cause: Throwable? = null) = F2Exception(
     error = this,
     cause
 )
+
+inline fun F2ErrorDTO.throwException() = throwException(null)
 
 inline fun F2ErrorDTO.throwException(cause: Throwable? = null) {
     throw asException(cause)
