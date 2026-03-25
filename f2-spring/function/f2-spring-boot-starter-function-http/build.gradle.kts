a31002ea69fe758b2a985d5c2f55f8026280326f
lheydel <luc.heydel@gmail.com>
2026-03-25 17:03:29 +0100

    DRY

--- f2-spring/function/f2-spring-boot-starter-function-http/build.gradle.kts
+++ f2-spring/function/f2-spring-boot-starter-function-http/build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
 dependencies {
     api(project(":f2-spring:function:f2-spring-boot-starter-function"))
     api(project(":f2-spring:exception:f2-spring-boot-exception-http"))
+    implementation(libs.spring.cloud.function.web)
 }
