2c3856be9b882c0f24963e02edfba746a84b209b
Adrien Poupard <adrien@komune.io>
2026-04-08 23:05:16 +0200

    Remove redundant maven-publish plugin and manual publication block in favor of fixers.gradle.publish

--- f2-gradle/f2-gradle-catalog/build.gradle.kts
+++ f2-gradle/f2-gradle-catalog/build.gradle.kts
@@ -1,21 +1,10 @@
 plugins {
     `version-catalog`
-    `maven-publish`
     alias(catalogue.plugins.fixers.gradle.publish)
 }
 
 catalog {
     versionCatalog {
         from(files("../../gradle/catalogue.versions.toml"))
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("versionCatalog") {
-            from(components["versionCatalog"])
-            artifactId = "f2-gradle-catalog"
-            groupId = "io.komune.f2"
-        }
     }
 }
